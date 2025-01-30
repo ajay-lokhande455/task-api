46c0cf0886569ae8f0fb8796eede2fd26f65351c
Ajay Lokhande <lokhandeajay455@gmail.com>
2025-01-30 20:45:25 +0530

    task added

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -1,4 +1,20 @@
-let tasks = []; // In-memory storage
+let tasks = [{
+    id: 0,
+    title: 'Task 1',
+    description: 'Description for Task 1'
+    },
+    {
+    id: 2,
+    title: 'Task 2',
+    description: 'Description for Task 2'
+    },
+    {
+    id: 3,
+    title: 'Task 3',
+    description: 'Description for Task 3'
+    }
+
+]; // In-memory storage
 let idCounter = 1;
 
 class Task {
